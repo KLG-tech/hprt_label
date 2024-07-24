3488b925547c4a8550affef6e62b7a747eb258fe
riyanwenas99 <riyan.wenas@kawanlamacorp.com>
2024-07-24 11:21:17 +0700

    first initialize project for HPRT Label plugins for Flutter (Android version)

--- lib/hprt_label.dart
+++ lib/hprt_label.dart
@@ -1,6 +1,19 @@
-
 import 'hprt_label_platform_interface.dart';
 
 class HprtLabel {
+  Future<bool?> connectIp(Map<String, dynamic> param) {
+    try {
+      return HprtLabelPlatform.instance.connectIp(param);
+    } catch (e) {
+      rethrow;
+    }
+  }
 
+  Future<String?> printImage(Map<String, dynamic> param) {
+    try {
+      return HprtLabelPlatform.instance.printImage(param);
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
