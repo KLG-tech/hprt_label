000678b0646313838a235b907a6c9d1cc9df92e7
riyanwenas99 <riyan.wenas@kawanlamacorp.com>
2024-07-25 10:58:52 +0700

    feat (print hprt by usb)

--- android/src/main/kotlin/com/klg/hprt_label/HprtLabelPlugin.kt
+++ android/src/main/kotlin/com/klg/hprt_label/HprtLabelPlugin.kt
@@ -3,6 +3,7 @@ package com.klg.hprt_label
 import android.app.PendingIntent
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -14,10 +15,10 @@ class HprtLabelPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
     private lateinit var channel: MethodChannel
     private lateinit var context: Context
 
-    /*//USB
+    //USB
     private lateinit var mUsbManager: UsbManager
+    private var device: UsbDevice? = null
     private lateinit var mPermissionIntent: PendingIntent
-    private var ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"*/
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         this.channel = MethodChannel(flutterPluginBinding.binaryMessenger, "hprt_label")
@@ -32,8 +33,10 @@ class HprtLabelPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
                 val ipAddress: String? = call.argument<String?>("ipAddress") ?: throw Exception("failed")
                 val port = "9100"
                 val resultConnect = HPRTPrinterHelper.PortOpen(context, "WiFi,$ipAddress,$port")
-                HPRTPrinterHelper.printAreaSize("62", "18")
-                HPRTPrinterHelper.Offset("0")
+                if (resultConnect == 0) {
+                    HPRTPrinterHelper.printAreaSize("64", "18")
+                    HPRTPrinterHelper.Offset("0")
+                }
                 if (resultConnect == 0) result.success("success") else result.success("failed")
             } else if (call.method.equals("printImage", true)) {
                 val byteData: ByteArray? = call.argument<ByteArray?>("byteData") ?: throw Exception("failed")
@@ -41,12 +44,41 @@ class HprtLabelPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
                 options.inMutable = true
                 val bmp = BitmapFactory.decodeByteArray(byteData, 0, byteData!!.size, options)
                 HPRTPrinterHelper.CLS()
-                HPRTPrinterHelper.printAreaSize("62", "18")
+                HPRTPrinterHelper.printAreaSize("64", "18")
                 HPRTPrinterHelper.Offset("0")
-                val resultPrint = HPRTPrinterHelper.printImage("62", "18", bmp, true, false, 0)
+                val resultPrint = HPRTPrinterHelper.printImage("64", "18", bmp, true, false, 0)
                 HPRTPrinterHelper.Print("1", "1")
                 if (HPRTPrinterHelper.IsOpened()) HPRTPrinterHelper.PortClose()
                 if (resultPrint == 0) result.success("success") else result.success("failed")
+            } else if (call.method.equals("connectUsb", true)) {
+                if (HPRTPrinterHelper.IsOpened()) HPRTPrinterHelper.PortClose()
+                mUsbManager = context.getSystemService(Context.USB_SERVICE) as UsbManager
+                val deviceList = mUsbManager.deviceList
+                val deviceIterator: Iterator<UsbDevice> = deviceList.values.iterator()
+                var havePrinter = false
+                while (deviceIterator.hasNext()) {
+                    val currentDevice = deviceIterator.next()
+                    if (currentDevice.deviceName.contains("hprt", true)) {
+                        device = deviceIterator.next()
+                        val count = device!!.interfaceCount
+                        for (i in 0 until count) {
+                            val intf = device!!.getInterface(i)
+                            if (intf.interfaceClass == 7) {
+                                havePrinter = true
+                                mUsbManager.requestPermission(device, mPermissionIntent)
+                            }
+                        }
+                    }
+                }
+                var resultConnect = -1
+                if (havePrinter && device != null) {
+                    resultConnect = HPRTPrinterHelper.PortOpen(context, device!!)
+                    if (resultConnect == 0) {
+                        HPRTPrinterHelper.printAreaSize("64", "18")
+                        HPRTPrinterHelper.Offset("0")
+                    }
+                }
+                if (resultConnect == 0) result.success("success") else result.success("failed")
             } else {
                 result.success("failed")
             }
